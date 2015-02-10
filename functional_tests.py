82284c5be377c9b644201916e133c2eb6101e729
Craig Johnson <cjohnson@cpp.com>
2015-02-09 20:46:08 -0700

    Updated FT to better handle list items

--- functional_tests.py
+++ functional_tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby 
         # is tying fly-fishing lures) 
-        inputbox.send_keys('Buy peacocks feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
@@ -42,13 +42,21 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical) 
-        self.fail('Finish the test!')
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys('Keys.Enter')
 
         # The page updates again, and now shows both items in her list 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some 
         # explanitory text to that effect. 
+        self.fail("Finish the test")
 
         # She visits that URL - her to-do list is still there. 
 

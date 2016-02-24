90ce7cd67afb7482201ee3ab51f605e18fbfcb5b
Craig Johnson <cjohnson@cpp.com>
2016-02-23 21:16:59 -0700

    Fixed failing tests

--- lists/views.py
+++ lists/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import redirect, render
-from lists.forms import ItemForm
-from lists.models import Item, List
-from django.core.exceptions import ValidationError
 
+from lists.forms import ExistingListItemForm, ItemForm
+from lists.models import List
 
 # Create your views here.
 
 def home_page(request): 
     return render(request, 'home.html', {'form': ItemForm()})
-    
-def view_list(request, list_id):
-    list_ = List.objects.get(id = list_id)
-    form = ItemForm()
-    if request.method == 'POST':
-        form = ItemForm(data = request.POST)
-        if form.is_valid():
-            form.save(for_list = list_)
-            return redirect(list_)
-    return render(request, 'list.html', {'list':list_, "form": form})
-
-
 
 def new_list(request): 
     form = ItemForm(data = request.POST)
@@ -29,3 +16,16 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id = list_id)
+    form = ExistingListItemForm(for_list = list_)
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list = list_, data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
+    return render(request, 'list.html', {'list':list_, "form": form})
+
+
+
